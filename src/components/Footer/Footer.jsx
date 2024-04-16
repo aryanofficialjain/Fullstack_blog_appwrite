1ca771a1f72492c7a240ed738dcaf19336e5f231
Aryan jain <aryanofficialjain.com>
2024-04-16 13:09:56 +0530

    complete the production grade compoenets

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>made by @aryanofficialjain</div>
+  )
+}
+
+export default Footer
---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = ({width="100%"}) => {
+  return (
+    <div>
+        <img src="" style={{width}} alt="Logo placeholder" />
+    </div>
+  )
+}
+
+export default Logo
